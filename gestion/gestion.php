4a8c9bf3c62d181c4d042a1c37684bec0e83c923
florianllimos <llimosflorian1@gmail.com>
2024-11-26 19:07:29 +0100

    feat: structure of the backoffice

---
+++ gestion/gestion.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once("../composants/utilisateur-connecte.php");
+
+require_once("../composants/header-gestion.php");
+
+require_once("../composants/background-fixed.php");
+
+require_once("../composants/navigation-gestion.php");
+
+?>
+
+<div class="bloc-white first-container-bloc">
+  <div class="try">
+    <img src="../media/favicon.png" alt="" class="card-1" />
+  </div>
+</div>
