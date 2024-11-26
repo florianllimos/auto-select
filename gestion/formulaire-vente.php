204e69ad51e06d586d1d2f37b77678bec09cfd88
florianllimos <llimosflorian1@gmail.com>
2024-11-26 19:06:59 +0100

    feat: component for manage the contact for a sell car

---
+++ gestion/formulaire-vente.php
@@ -0,0 +1,79 @@
+<?php
+
+require_once("../composants/utilisateur-connecte.php");
+
+require_once("../composants/database.php");
+
+require_once("../composants/header-gestion.php");
+
+require_once("../composants/background-fixed.php");
+
+require_once("../composants/navigation-gestion.php");
+
+$sql = "SELECT * FROM formulaire_vente";
+$requete = $db->query($sql);
+$formulaires = $requete->fetchAll();
+
+
+?>
+
+<div class="container-vente">
+  
+  <?php
+
+    foreach($formulaires as $formulaire): 
+    
+  ?>
+
+  <form class="form" method="POST">
+    <h3 class="title-form"><?= $formulaire["vehicule"] ?></h3>
+    <div class="bloc-form">
+      <input type="hidden" class="center" name="ide" id="ide" value="<?= $formulaire["id"] ?>">
+    </div>
+    <div class="bloc-form">
+      <p class="black">Voiture : <?= $formulaire["vehicule"] ?></p>
+    </div>
+    <div class="bloc-form">
+      <p class="black">Nom : <?= $formulaire["nom"] ?></p>
+    </div>
+    <div class="bloc-form">
+      <p class="black">Téléphone : 0<?= $formulaire["telephone"] ?></p>
+    </div>
+    <button type="submit" class="delete" name="delete" value="<?= $formulaire["id"] ?>">Supprimer cette demande de contact</button>
+
+    <?php
+
+      if($_SERVER['REQUEST_METHOD'] === 'POST') {
+      
+        if(isset($_POST["delete"])) {
+      
+          $id = $_POST["delete"];
+          $deletesql = "DELETE FROM formulaire_vente WHERE id = :id";
+          $stmt = $db->prepare($deletesql);
+          $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+      
+          if($stmt->execute()) {
+              
+            echo "<h2 class='success'>Formulaire supprimé</h2>";
+                    
+          } else {
+      
+            echo "<h2 class='error'>Erreur lors de la suppresion du formulaire</h2>";
+      
+          }   
+      
+        }
+      
+      }
+
+    ?>
+
+  </form>
+
+  <?php
+
+endforeach; 
+
+  ?>
+
+</div>
