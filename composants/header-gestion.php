7965b195f75433edcaa6179ed68ad73c159f5f7c
florianllimos <llimosflorian1@gmail.com>
2024-11-26 18:54:51 +0100

    add: header for user connect

---
+++ composants/header-gestion.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8"> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta
+      name="description"
+      content="Vincent Parrot, garagiste depuis plusieurs année. Vente de véhicules."
+    />
+  <title>Auto Select</title>
+  <link rel="icon" type="image/x-icon" href="../media/favicon.png">
+  <link rel="stylesheet" href="../CSS/style.css">
+  <link rel="stylesheet" href="../CSS/BackgroundFixed.css">
+  <link rel="stylesheet" href="../CSS/Footer.css">
+</head>
+<body>
+  <header class="header">
+    <nav class="navbar container">
+      <h1>Auto Select</h1>
+      <input type="checkbox" id="toggler" />
+      <label for="toggler"><img src="../media/burger.png" class="burgerLogo"></img></label>
+      <div class="menu">
+        <ul class="list">
+          <a href="../index.php"><li class="LiNavTop">Accueil</li></a>
+          <a href="../reparation.php"><li class="LiNavTop">Réparation</li></a>
+          <a href="../vente.php"><li class="LiNavTop">Vente</li></a>
+          <a href="../contact.php"><li class="LiNavTop">Contact</li></a>
+          <a href="gestion.php"><li class="LiNavTop">Gestion</li></a>
+        </ul>
+      </div>
+    </nav>
+  </header>
